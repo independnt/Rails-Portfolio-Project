f195fa1742c0ec64b722bf68fabec635ef5bbc00
independnt <gonzalezdennis6@gmail.com>
2018-04-03 17:19:04 -0400

    added phone_number to users table. Included new.html and _form in users folder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403161658) do
+ActiveRecord::Schema.define(version: 20180403211731) do
 
   create_table "backed_projects", force: :cascade do |t|
     t.integer "user_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180403161658) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
   end
 
 end
