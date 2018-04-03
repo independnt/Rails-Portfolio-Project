7d56da7b69e588f70d5ad2f88d9ee528cf3fd040
independnt <gonzalezdennis6@gmail.com>
2018-04-03 14:28:47 -0400

    added has_secure_password and validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :projects
   has_many :backed_projects
   has_many :projects_backed, through: :backed_projects, source: 'project'
+  validates :username, presence: true
+  validates :email, presence: true
 end
