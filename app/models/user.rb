b8c808004cba26e7361f62da62b8042c7c05d2dd
independnt <gonzalezdennis6@gmail.com>
2018-04-03 14:50:11 -0400

    added add_project method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   has_many :projects_backed, through: :backed_projects, source: 'project'
   validates :username, presence: true
   validates :email, presence: true
+
+  def add_project(project)
+    if self.projects_backed.where(id: project.id).empty?
+      BackedProject.create(article: project)
+    end
+  end
+
 end
