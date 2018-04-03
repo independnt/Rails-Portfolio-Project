539b8047840ea4ec809caf3f618426aaa217f8ed
independnt <gonzalezdennis6@gmail.com>
2018-04-03 16:54:04 -0400

    updated routes for users and sessions controller. Added folders for controllers and html for welcome#home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 
   root "welcome#home"
 
+  resource :sessions, only:[:new, :create, :destroy]
+
+  resources :users do
+    resources :projects, only:[:new, :create, :show, :index]
+  end
 
 end
