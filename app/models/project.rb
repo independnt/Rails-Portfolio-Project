3d44ab618e9f25bb5e768cb1d40c168a67daa745
independnt <gonzalezdennis6@gmail.com>
2018-04-04 19:05:29 -0400

    added categories_attributes= method to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,13 @@ class Project < ApplicationRecord
   has_many :categories, through: :project_categories
   has_many :backed_projects
   has_many :backed, through: :backed_projects, source: 'user'
-  accepts_nested_attributes_for :categories 
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+  
 end
