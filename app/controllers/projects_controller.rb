0317a266076d8e4e56c50d72cbf2ec8be6299dc7
independnt <gonzalezdennis6@gmail.com>
2018-04-05 16:08:20 -0400

    fixed project form and users helper method project_list. fixed project controller for create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,13 +5,25 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to user_project_path(@project.user_id, @project)
+    else
+      render :new
+    end
+  end
 
+  def show
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      @project = @user.projects.find_by(id: params[:id])
+    end
   end
 
   private
 
   def project_params
-    params.require(:project).permit(:name, :description, :city, :state, :user_id, :category_ids:[], categories_attributes:[:name])
+    params.require(:project).permit(:name, :description, :city, :user_id, :state, category_ids:[], categories_attributes:[:name])
   end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
 
   def project_list(user)
+    link = ""
     user.projects.each do |project|
-      link_to "project.name", user_project_path(user, project)
+      link = link_to(project.name, user_project_path(user, project))
     end
+    link
   end
 
 end
