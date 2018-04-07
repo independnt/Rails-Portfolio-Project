597d5129cb085037ce12056210c066447ad405fd
independnt <gonzalezdennis6@gmail.com>
2018-04-07 14:16:59 -0400

    additional helper methods, more html to show pages for certain users

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -57,4 +57,13 @@ module ProjectsHelper
     ]
 end
 
+
+  def edit_project(user, project)
+    link_to "Edit Project", edit_user_project_path(user, project)
+  end
+
+  def delete_project(user, project)
+    link_to "Delete Project", user_delete_project_path(user, project), method: :delete, confirm: "Are you sure?"
+  end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,19 +1,15 @@
 module UsersHelper
 
-  def edit_project(user, project)
-    link_to "Edit Project", edit_user_project_path(user, project)
-  end
-
-  def delete_project(user, project)
-    link_to "Delete Project", user_delete_project_path(user, project), method: :delete, confirm: "Are you sure?"
-  end
-
   def correct_user(user)
     user == current_user
   end
 
   def guest_user(user)
     user != current_user
+  end
+
+  def backed_user(user, project)
+    user.projects_backed.include?(project)
   end
 
 end
