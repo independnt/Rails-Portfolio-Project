1d238dff9b598bbe4376f05fc15655224472c098
independnt <gonzalezdennis6@gmail.com>
2018-05-14 17:28:51 -0400

    added ajax functionality to get more information on projects using API

---
+++ app/assets/javascripts/project.js
@@ -0,0 +1,12 @@
+$(function(){
+  $(".js-more").on("click", function(){
+    let project_id = $(this).data("id");
+    let user_id = $(this).data("uid")
+    $.get("/users/" + user_id + "/projects/" + project_id + ".json", function(data){
+      $("#project-display").html(
+        `<h3>${data["name"]}</h3>
+        <p>${data["description"]}</p>`
+      )
+    })
+  })
+})
