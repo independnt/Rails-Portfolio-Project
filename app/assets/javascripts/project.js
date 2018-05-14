960a820015e9d134bcc0764df5d7390e472894d0
independnt <gonzalezdennis6@gmail.com>
2018-05-14 17:52:58 -0400

    added spec.js.md file

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -2,7 +2,7 @@ $(function(){
   $(".js-more").on("click", function(){
     let project_id = $(this).data("id");
     let user_id = $(this).data("uid")
-    $.get("/users/" + user_id + "/projects/" + project_id + ".json", function(data){
+    $.get("/users/" + user_id + "/projects/" + project_id + ".json", data =>{
       $("#project-display").html(
         `<h3>${data["name"]}</h3>
         <p>${data["description"]}</p>`
